bce07380ed885b323818d2ab347ae0437fa6a2f9
vnino2003 <vincent.edu.mail@gmail.com>
2025-10-06 14:11:36 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -74,9 +74,9 @@ $database['main'] = array(
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
 
-    'username'	=> 'sql12800395',
-    'password'	=> 'C4jtkGbRUj',
-    'database'	=> 'sql12800395',
+    'username'	=> 'sql12800501',
+    'password'	=> 'muFM1QWIEU',
+    'database'	=> 'sql12800501',
     'charset'	=> 'utf8',
     'dbprefix'	=> '',
     // Optional for SQLite
